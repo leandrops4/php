297f29948f5cec532679dd2b3b1ed2034b55af3f
leandrops4 <56371687+leandrops4@users.noreply.github.com>
2019-11-14 10:33:59 -0300

    traduzi o texto
    traduzi o texto e tirei uns códigos a mais que fizeram, sei la o que ocorreu...

--- index.php
+++ index.php
@@ -1,23 +1,8 @@
-<<<<<<< HEAD
+
 <?php
  
- echo "hello world";
+ echo "Olá Mundo";
  
 ?>
-=======
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-</head>
-<body>
 
-<?php
 
- echo "Meu primeiro <strong>código</strong> PHP"; 
-
- ?>
-
- </body>
-</html>
->>>>>>> 78f64e2edbf33be21967111efbc41e7483ac63fa
